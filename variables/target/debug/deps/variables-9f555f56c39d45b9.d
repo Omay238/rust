d13499bdf1f8d35c8dff06e2e18e4424714104f1
Omay <70179858+Omay238@users.noreply.github.com>
2022-12-03 23:15:59 +0000

    chapter 3

---
+++ variables/target/debug/deps/variables-9f555f56c39d45b9.d
@@ -0,0 +1,5 @@
+/workspaces/rust/variables/target/debug/deps/variables-9f555f56c39d45b9: src/main.rs
+
+/workspaces/rust/variables/target/debug/deps/variables-9f555f56c39d45b9.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/workspaces/rust/variables/target/debug/variables: /workspaces/rust/variables/src/main.rs
